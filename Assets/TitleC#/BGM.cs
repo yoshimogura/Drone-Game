866f2583f2db554402a66cc5293856eede267343
yoshimogura <[200~nakatayoshifumi3@gmail.com~>
2025-11-10 19:39:39 +0900

    BGM停止機能も付けた

--- Assets/TitleC#/BGM.cs
+++ Assets/TitleC#/BGM.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class BGM : MonoBehaviour
 {
+    private AudioSource audioSource;
+
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);//
+        // 重複防止
+        if (FindObjectsOfType<BGM>().Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Result") 
+        {
+            audioSource.Stop();
+        }
+    }
+
+    
 
 }
